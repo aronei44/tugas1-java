738e4f454c04db21c9e521d7e9e93985f9409d52
aronei44 <meliodasmeliodas224@gmail.com>
2024-05-04 07:25:55 +0700

    feat (docs)

--- src/main/java/com/tugas1/Main.java
+++ src/main/java/com/tugas1/Main.java
@@ -4,10 +4,27 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        // 1
-        int jumlahBaris;
-        String kalimatBaru;
+        // 1 Buatlah sebuah deklarasi variabel dengan tipe data integer yang bernama ‘jumlahBaris’?
+        int jumlahBaris = 1;
+
+        System.out.println("==== Soal 1 ====");
+        System.out.println(jumlahBaris);
+
+        // 2 Buatlah sebuah deklarasi variabel dengan tipe data string yang bernama ‘kalimatBaru’ yang berisi kata ‘Deklarasi tipe data String’?
+        String kalimatBaru = "Deklarasi tipe data String";
+
+        System.out.println("==== Soal 2 ====");
+        System.out.println(kalimatBaru);
+
+        // 3 Buatlah pendeklarasian array satu dimensi dengan nama ‘empatAngka’, tipe data integer, yang berisi angka (07, 10, 20, 23)? Pendeklarasian tersebut dengan menggunakan bahasa pemrograman Java.
         int empatAngka[] = {7, 10, 20, 23};
+
+        System.out.println("==== Soal 3 ====");
+        for (int i = 0; i < empatAngka.length; i++) {
+            System.out.println(empatAngka[i]);
+        }
+
+        // 4 Buatlah pendeklarasian array dua dimensi dengan nama ‘alfabet’, tipe data string, yang berisi huruf (p, s, n), (w, l, b), (f, r, e)? Pendeklarasian tersebut dengan menggunakan bahasa pemrograman Java.
         String[][] alfabet = new String[3][3];
         alfabet[0][0] = "p";
         alfabet[0][1] = "s";
@@ -18,6 +35,16 @@ public class Main {
         alfabet[2][0] = "f";
         alfabet[2][1] = "r";
         alfabet[2][2] = "e";
+
+        System.out.println("==== Soal 4 ====");
+        for (int i = 0; i < alfabet.length; i++) {
+            for (int j = 0; j < alfabet[i].length; j++) {
+                System.out.print(alfabet[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // 5 Buatlah pendeklarasian linked list dengan nama ‘listAngka’, tipe data integer, yang berisi angka (26, 8, 23, 24, 16)? Pendeklarasian tersebut dengan menggunakan bahasa pemrograman Java.
         LinkedList<Integer> listAngka = new LinkedList<>();
         listAngka.add(26);
         listAngka.add(8);
@@ -25,6 +52,9 @@ public class Main {
         listAngka.add(24);
         listAngka.add(16);
 
-        System.out.print(empatAngka[0] + " ");
+        System.out.println("==== Soal 5 ====");
+        for (int i = 0; i < listAngka.size(); i++) {
+            System.out.println(listAngka.get(i));
+        }
     }
 }
