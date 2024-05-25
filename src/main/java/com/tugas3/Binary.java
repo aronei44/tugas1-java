035af91b251d36ca4a885035ff02b038b7f38edc
aronei44 <meliodasmeliodas224@gmail.com>
2024-05-25 07:02:32 +0700

    feat (tugas 3)

---
+++ src/main/java/com/tugas3/Binary.java
@@ -0,0 +1,58 @@
+package com.tugas3;
+
+public class Binary {
+
+    static int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 7, 3, 5, 7, 9, 10, 11, 1, 2 ,3 ,4, 5, 5, 8, 9 };
+
+        int target = 10;
+
+        arr = bubbleSort(arr);
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        int result = binarySearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+        
+    }
+}
---
+++ src/main/java/com/tugas3/Sequential.java
@@ -0,0 +1,28 @@
+package com.tugas3;
+
+public class Sequential {
+
+    static int sequentialSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 7, 3, 5, 7, 9 };
+
+        int target = 5;
+
+        int result = sequentialSearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+        
+    }
+}
